532069f8846db02e4f682e820e641cb7bcafa4ed
Robyn <clockhands9@gmail.com>
2022-10-31 15:42:39 +0000

    add basic header layout and get articles

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,7 +1,20 @@
-import { CurrentUser } from "./Cards/CurrentUser"
+import { useEffect, useState } from "react";
+import { CurrentUser } from "./Cards/CurrentUser";
+import * as API from '../Api';
 
 
 export const Header = () => {
+    const [topics, setTopics] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+
+    useEffect(() => {
+        setIsLoading(true)
+        API.fetchTopics()
+        .then(({topics}) => {
+            setTopics(topics);
+            setIsLoading(false);
+        })
+    },[])
 
     return (
     <div className="header">
@@ -10,8 +23,16 @@ export const Header = () => {
         <CurrentUser />
         </section>
         <section className="header-bottom">
-            {'topics map'}
-            <input placeholder="Search" type="text"/>
+            <ul className="topic-list">
+            {topics.map(topic => {
+                const { slug, description } = topic;
+                return (
+                <h3 className="topic" key={slug}>{slug}</h3>
+                )
+            })}
+            <h3>|</h3>
+            </ul>
+            <input className="article-search" placeholder="Search" type="text"/>
         </section>
     </div>
     
