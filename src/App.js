7e93d204ea05309eb5aceb1850bba458ef7ba0da
Robyn <clockhands9@gmail.com>
2022-11-01 10:55:54 +0000

    routes refactor

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path='/articles/:topic' element={<TopicsLanding />}/>
-          <Route path='/home' element={<Home />}/>
+          <Route path='/' element={<Home />}/>
         </Routes>
       </BrowserRouter>
     </div>
