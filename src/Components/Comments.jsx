7ecea08e042334b0e0abb7e60a9a6439c07a6cd5
Robyn <clockhands9@gmail.com>
2022-11-03 16:30:27 +0000

    comment input styling and handle submit

--- src/Components/Comments.jsx
+++ src/Components/Comments.jsx
@@ -4,6 +4,8 @@ import { useEffect, useState } from 'react';
 export const Comments = (article_id) => {
     const [comments, setComments] = useState([]);
     const [isLoading, setIsLoading] = useState(true);
+    const [input, setInput] = useState('')
+    const [newComment, setNewComment] = useState(null)
 
 
     useEffect(() => {
@@ -13,18 +15,36 @@ export const Comments = (article_id) => {
             setIsLoading(false);
             setComments(comments)
         })
+    },[])
 
+    const handleChange = (event) => {
+        const input = event.target.value;
+        setInput(input);
+        if(event.key === 'Enter') handleSubmit(input);
+    }
+    const handleSubmit = (input) => {
+        setComments()
+    }
+
+    useEffect(() => {
+        API.postComment()
+        .then(({comment}) => {
+            const newComments = [...comments];
+            newComments.unshift(comment);
+            setComments(newComments);
+        })
     },[])
     
 
     return (
-        <div>
-            <input type='text'></input>
-            <section className="comment-list">
-                {comments.map(comment => {
-                    return <CommentCard comment={comment}/>
-                })}
-            </section>
-        </div>
+    <div className="comment-container">
+        <label className="comment-label" for="comment">Write a comment:</label>
+        <input onChange={handleChange} id="comment" className='comment-input' type="text"></input>
+        <section className="comment-list">
+            {comments.map(comment => {
+                return <CommentCard comment={comment}/>
+            })}
+        </section>
+    </div>
     )
 }
